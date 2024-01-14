5ec028afc5ddb5ebc9cb83c38130fbf5b48b393f
TCCtech Julatip Nuwan <Julatip.N@tcc-technology.com>
2024-01-14 09:46:56 +0700

    add let variable

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-console.log("hello");
-console.log("Julatip Nuwan");
+
+// console.log("hello boss");
+// console.log("Julatip Nuwan");
+// console.log(30);
+
+// let Name ="Julatip Nuwan";
+// let Age=30;
+// console.log(Name, Age);
+
+let firstName="Nuwan";
+let lastName="Nuwan";
+let age=30;
+console.log(firstName, lastName , age);
