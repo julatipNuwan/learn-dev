b1f003384318f42d36df63a9207ed8d2f231bb93
TCCtech Julatip Nuwan <Julatip.N@tcc-technology.com>
2024-01-14 11:03:37 +0700

    add Greeting

--- cafe.js
+++ cafe.js
@@ -1,7 +1,17 @@
 let cafeName ="enjoy cafe";
 let Name = "Biew";
 
-message ="welcome" +" "+ cafeName;
-orderReadyMessage ="เครื่องดื่ม คุณ" +" " + Name +" "+ "ได้แล้วค่ะ"
+let message ="welcome" +" "+ cafeName;
+let Greeting = cafeName +" " + "สวัสดีค่ะ" ;
+let orderReadyMessage ="เครื่องดื่ม คุณ" +" " + Name +" "+ "ได้แล้วค่ะ";
 console.log(message);
 console.log(orderReadyMessage);
+console.log(Greeting);
+console.log("--------------------------------------");
+
+Greeting = "welcome to" +" "+ cafeName;
+Name = "Re"
+orderReadyMessage ="เครื่องดื่ม คุณ" +" " + Name +" "+ "ได้แล้วค่ะ";
+console.log(Greeting);
+console.log(orderReadyMessage);
+ 
