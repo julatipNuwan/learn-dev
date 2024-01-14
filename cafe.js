0509407140fc4732b42f9548d4cc5884bbbb80fc
TCCtech Julatip Nuwan <Julatip.N@tcc-technology.com>
2024-01-14 10:35:01 +0700

    add cafe

---
+++ cafe.js
@@ -0,0 +1,7 @@
+let cafeName ="enjoy cafe";
+let Name = "Biew";
+
+message ="welcome" +" "+ cafeName;
+orderReadyMessage ="เครื่องดื่ม คุณ" +" " + Name +" "+ "ได้แล้วค่ะ"
+console.log(message);
+console.log(orderReadyMessage);
