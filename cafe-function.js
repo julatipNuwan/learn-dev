2a9141eac1a396daa2265e20499b73963f852097
TCCtech Julatip Nuwan <Julatip.N@tcc-technology.com>
2024-01-14 12:00:30 +0700

    add function

--- cafe-function.js
+++ cafe-function.js
@@ -1,8 +1,32 @@
-function sayWelcome(){
-    console.log("Welcome จ้า");
-}
+//แบบที่ 1 
+// function sayWelcome(){
+//     console.log("Welcome จ้า");
+// }
 
-sayWelcome();
-sayWelcome();
-sayWelcome();
-sayWelcome();
+// sayWelcome();
+// sayWelcome();
+// sayWelcome();
+// sayWelcome();
+
+//แบบที่ 2 
+// function sayWelcome(customerName){
+//     console.log("Welcome " + customerName);
+// }
+// sayWelcome("Air");
+// sayWelcome("Aa");
+// sayWelcome("Ann");
+// sayWelcome("Biew");
+
+//แบบที่ 3 
+function welcome(welcomeMeassge){
+    console.log("welcome" +" "+ welcomeMeassge);
+}
+welcome(": Biew");
+welcome(": Ann");
+
+
+function sayWelcome(customerName){
+    console.log("เครื่องดื่ม คุณ" +" " + customerName +" "+ "ได้แล้วค่ะ");
+}
+sayWelcome("Biew");
+sayWelcome("Ann");
